e372eecdb6cd89b5c9a7a2ec45ad308e67cfcac3
malhotra-parul <parulm.business@gmail.com>
2020-03-29 10:07:39 +0530

    Added a new state property- language to hold votes for individual languages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.styles.css";
+import PollingScreen from "./components/PollingScreen";
+
+class App extends Component{
+    
+
+    render(){
+        return(
+            <PollingScreen />
+        );
+    }
+
+};
+
+export default App;
