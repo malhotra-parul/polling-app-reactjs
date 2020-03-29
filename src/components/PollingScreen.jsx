938088841dc671773e88189cd44b89caee75cecd
malhotra-parul <parulm.business@gmail.com>
2020-03-29 13:12:45 +0530

    Displaying percentages of polling on submit button

--- src/components/PollingScreen.jsx
+++ src/components/PollingScreen.jsx
@@ -20,52 +20,71 @@ class PollingScreen extends Component {
 
     onSubmit =  (e)=>{
         e.preventDefault();
+        
         const language = this.state.languages.reduce((acc, language)=>{
             if(this.state.selected === language.name){
                 language.votes = language.votes+1;
             }
             return [...acc, language];
-        
-        }, []);      
-    
+        }, []);
+
         this.setState({
             selected : "", languages: language
-        });
-        console.log(this.state.languages[2]);
+        }); 
     }
     onChange = (e)=>{
         this.setState({
-            selected: e.target.value,
-            
+            selected: e.target.value
         });
-    }
+    };
+
     render(){
+        const totalVotes = this.state.languages.reduce((acc, lang)=>{
+            return acc + lang.votes;
+        }, 0)
+
+        console.log(totalVotes);
+     
     return(
+        
+       
         <div className="container">
             <div className="polling-list">
-            <h2>Which is your favourite framework?</h2>
+            <h2>Which is your favourite?</h2>
             <form onSubmit={this.onSubmit}>
-                <div className="option"><label><input type="radio" 
+                <div className="option"><label><input 
+                                                type="radio" 
                                                 value="Vanilla JS" 
                                                 onChange={this.onChange}
-                                                checked={this.state.selected === "Vanilla JS"} />Vanilla JS</label>
-                                                <span className="votes">{this.state.languages[0].votes}</span></div>
-                <div className="option"><label><input type="radio" 
+                                                checked={this.state.selected === "Vanilla JS"} 
+                                                />Vanilla JS</label>
+                                                <span className="votes">{
+                                                    (totalVotes) ?
+                                                    ((parseInt(this.state.languages[0].votes)/totalVotes)*100).toFixed(2)+"%" : 0}</span></div>
+                <div className="option"><label><input className="checked"
+                                                      type="radio" 
                                                       value="Angular JS" 
                                                       onChange={this.onChange}
                                                       checked={this.state.selected === "Angular JS"}/>Angular JS</label>
-                                                      <span className="votes">{this.state.languages[1].votes}</span></div>
-                <div className="option"><label><input type="radio" 
+                                                      <span className="votes">{
+                                                        (totalVotes) ?
+                                                        ((parseInt(this.state.languages[1].votes)/totalVotes)*100).toFixed(2)+"%" : 0}</span></div>
+                <div className="option"><label><input className="checked" type="radio" 
                                                       value="React JS" 
                                                       onChange={this.onChange}
                                                       checked={this.state.selected === "React JS"}/>React JS</label>
-                                                      <span className="votes">{this.state.languages[2].votes}</span></div>
-                <div className="option"><label><input type="radio" 
+                                                      <span className="votes">{
+                                                        (totalVotes) ?
+                                                        ((parseInt(this.state.languages[2].votes)/totalVotes)*100).toFixed(2) + "%" : 0}</span></div>
+                <div className="option"><label><input className="checked" type="radio" 
                                                       value="Vue JS" 
                                                       onChange={this.onChange}
                                                       checked={this.state.selected === "Vue JS"}/>Vue JS</label>
-                                                      <span className="votes">{this.state.languages[3].votes}</span></div>
-               <button type="submit">Submit Vote!</button>
+                                                      <span className="votes">{
+                                                        (totalVotes) ?
+                                                        ((parseInt(this.state.languages[3].votes)/totalVotes)*100).toFixed(2) + "%" : 0}</span></div>
+               
+                                                      <button type="submit">Submit Vote!</button>
             </form>
             </div>
             
